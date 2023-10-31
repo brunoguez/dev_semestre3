79246d2689a9a68a941e4874597277da4c5c9ae1
brunoguez <brunomguez@gmail.com>
2023-10-30 21:49:05 -0300

    auth

--- TecProgr2/escola/src/app.js
+++ TecProgr2/escola/src/app.js
@@ -4,13 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const rota_turma = require('./routes/rota_turma');
 const rota_aluno = require('./routes/rota_aluno');
+const rota_auth = require('./routes/rota_auth');
+const session = require('express-session');
 
+app.use(session({
+    secret: 'seuSegredo',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home');
 });
 
@@ -19,6 +26,9 @@ app.use('/rota_turma', rota_turma);
 
 //Remanejando Rotas do aluno 
 app.use('/rota_aluno', rota_aluno);
+
+app.use('/auth', rota_auth);
+
 const PORT = 8081;
 app.listen(PORT, () => {
     console.log("Servidor Rodando");
